98f77ea4fa78888eb200d795c96eaaa709a522e5
Henry Ly <160989916+henryhlly@users.noreply.github.com>
2026-04-27 20:12:45 +1200

    Feature/payload setup (#32)
    * Added backend collections and put in config
    
    * Payload types update
    
    * Resolved copilot issues

---
+++ cms/src/collections/Members.ts
@@ -0,0 +1,57 @@
+import type { CollectionConfig } from 'payload'
+
+export const Members: CollectionConfig = {
+  slug: 'members',
+  auth: true,
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'phone',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'status',
+      type: 'select',
+      options: [
+        {
+          label: 'ACTIVE',
+          value: 'active'
+        },
+        {
+          label: 'EXPIRED',
+          value: 'expired'
+        },
+        {
+          label: 'PENDING',
+          value: 'pending'
+        },
+      ],
+      required: true,
+      defaultValue: 'pending',
+    },
+    {
+      name: 'membershipExpiryDate',
+      type: 'date',
+    },
+    {
+      name: 'stripeCustomerId',
+      type: 'text',
+    },
+    {
+      name: 'emergencyContactName',
+      type: 'text',
+    },
+    {
+      name: 'emergencyContactPhone',
+      type: 'text',
+    }
+  ]
+}
