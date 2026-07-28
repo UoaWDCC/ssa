ffaf5104afe5e1fa668d21c4fb2a53878a25d3bc
GladwynChua <gladwynchua@gmail.com>
2026-07-28 12:27:01 +1200

    fix: resolve eslint error

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -37,6 +37,7 @@ export default function Navbar() {
   }, [menuOpen])
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setHidden(window.scrollY > 80)
 
     let lastY = window.scrollY
