f07cc44e6f424d1e6311b5776377906872a9046b
Becky <beckyngan28@gmail.com>
2026-08-12 21:28:47 +1200

    fix: fixing card title error

--- web/src/components/CardSection.tsx
+++ web/src/components/CardSection.tsx
@@ -1,11 +1,17 @@
 export default function CardSection({
+  title,
   children,
 }: {
+  title?: string
   children: React.ReactNode
 }) {
   return (
     <div className=" bg-ssa-yellow-light rounded-2xl p-6 md:p-8 flex flex-col gap-5 shadow-md">
-      <div></div>
+      <div>
+        <h2 className="font-averia font-bold text-xl md:text-2xl lg:text-3xl text-ssa-black">
+          {title}
+        </h2>
+      </div>
       {children}
     </div>
   )
