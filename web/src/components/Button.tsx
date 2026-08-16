bfc4babc119160546f7249eec0a55345210ec89d
Henry Ly <lyhenry2004@gmail.com>
2026-08-16 14:20:54 +1200

    Fixed button color build error

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -4,7 +4,13 @@ import { FiArrowRight } from 'react-icons/fi'
 
 export type ButtonSize = 'short' | 'long'
 export type ButtonVariant = 'filled' | 'light' | 'outline'
-export type ButtonColor = 'red' | 'red-light' | 'yellow' | 'skin' | 'salmon'
+export type ButtonColor =
+  | 'red'
+  | 'red-light'
+  | 'yellow'
+  | 'skin'
+  | 'salmon'
+  | 'white'
 export type ArrowSide = 'left' | 'right'
 
 /**
